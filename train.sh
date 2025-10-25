f741c8394d4a62a6c68ae29eb2e9c8f26bf3313d
Confidential <109396645+cookfu@users.noreply.github.com>
2025-10-25 10:01:44 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,4 +1,2 @@
 export PYTHONPATH="$PYTHONPATH:$(pwd)"
-CUDA_VISIBLE_DEVICES=0 python basicsr/train.py -opt /root/shared-nvme/WaveMamba/options/train_mamba.yml
-# CUDA_VISIBLE_DEVICES=0 python basicsr/train.py -opt /root/shared-nvme/Wave-Mamba-main/options/train_wavemamba_lolv2_real.yml
-# CUDA_VISIBLE_DEVICES=0 python basicsr/train.py -opt /root/shared-nvme/Wave-Mamba-main/options/train_wavemamba_lolv2_syn.yml
+CUDA_VISIBLE_DEVICES=0 python basicsr/train.py -opt options/train_mamba.yml
